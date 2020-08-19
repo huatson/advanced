6322672fa1c34e638aeb431852ff7c62b8db78b0
huatson <huatson@live.com>
2020-08-19 14:03:21 -0700

    updated pairs values

--- src/advanced.cpp
+++ src/advanced.cpp
@@ -13,291 +13,117 @@
 
 #include <unordered_map>
 
-bool solveDFS()
-{
-	cout << "init stuff\n";
-	CustomDFS *dfs = new CustomDFS();
-	if (!dfs)
-	{
-		cerr << "Error, creating instance\n";
-		return false;
-	}
-	const char *fpath = "CustomDFS/input.txt";
-	TreeNode *node = dfs->createTreeNode(fpath);
-	if (!node)
-	{
-		cerr << "Error, creating node\n";
-		delete dfs;
-		return false;
-	}
-	vector<vector<int>> order = dfs->levelOrder(node);
-	if (order.empty())
-		return false;
-	return true;
-	delete dfs;
-}
-
-bool compareLists()
-{
-	int a[] = {1, 8, 9, 20};
-	int b[] = {5, 8, 20, 1};
-	std::vector<int> out;
-	// contador de lista a
-	int countera[21] = {0};
-	// contador de lista b
-	int counterb[21] = {0};
-	for (unsigned int i = 0; i < 4; i++)
-	{
-		countera[a[i]]++;
-		counterb[b[i]]++;
-	}
-
-	for (unsigned int j = 0; j < 21; j++)
-	{
-		//	excluir coincidencias o no existentes
-		if ((countera[j] == 1 && counterb[j] == 1) || (countera[j] == 0 && counterb[j] == 0))
-		{
-			continue;
-		}
-		out.push_back(j);
-	}
-	return true;
-}
-
 /**
- * target = 8
+ *	a=[10,20,30,20,20,50,50,10,70]
+ *	output=3
+ *	vacio[]
+ *	numeros enteros
+ *	0<=a[i]<=100
  * 
- * a=[1,2,3,9]
+ *	iterativo:
+ *	a[i], a[j] >= 0 && i != j
+ *	10 == 20 --> false
+ *	10 == 30 --> false
+ *		...
+ *	10 == 10 --> true
+ *	a=[-1,20,30,20,20,50,50,-1,70]
+ *	20 == 30 --> false
+ *	20 == 20 --> true
+ *	a=[-1,-1,30,-1,20,50,50,-1,70]
+ *	30 == 20 --> false
+ *	30 == 50 --> false
+ *	30 == 50 --> false
+ *	30 == 70 --> false
+ *	20 == 30 --> false
+ *		...
+ *	50 == 30 --> false
+ *	50 == 20 --> false
+ *	50 == 50 --> true
+ *	a=[-1,-1,30,-1,20,-1,-1,-1,70]
+ *	70 == 30 --> false
+ *	70 == 20 --> false
+ *
+ * contador: 
+ * crear contador c de a
+ * (c >= 2)
+ * 0 >= 2 --> false: NEXT
+ * 2 >= 2 --> true: 2/2 = 1
+ * 3 >= 2 --> true: (3-1)/2 = 1
+ * 4 >= 2 --> true: 4/2= 2 
+ * ..
  * 
- * nCr --> n = 4, r = 2
- * [1+2] --> combinacion
- * 1+2 == 8 --> false
- * 1+3 == 8 --> false
- * 1+9 == 8 --> false
- * 
- * 2+3 == 8 --> false
- * 2+9 == 8 --> false
- * 
- * 3+9 == 8 --> false
- * 
- * b=[1,2,4,4]
- * 
- * 1+2 == 8 --> false
- * 1+4 == 8 --> false
- * 1+4 == 8 --> false
- * 
- * 2+4 == 8 --> false
- * 2+4 == 8 --> false
- * 
- * 4+4 == 8 --> true
- * 
- * example:
- * vector<int> listA = {1,2,3,9};
- * vector<int> listB = {1,2,4,4};
-**/
-#define TARGET 8
-vector<int> c;
-bool result = false;
-int counter = 0;
-bool testSum(vector<int> &c)
+ **/
+bool IsValidPair(vector<int> &a, const int i, const int j)
 {
-	int sum = 0;
-	for (int k = 0; k < c.size(); ++k)
-	{
-		sum += c[k];
-	}
-	if (sum == TARGET)
-	{
-		return true;
-	}
-	return false;
-}
-void comparesum(vector<int> &list, int stride, int r)
-{
-	// nCr: Si n = #total de elementos en una lista (n=4),
-	// y r = #elementos de la lista n, que tiene que contener cada k-combinacion (r=2)
-	// Entonces: Si solo cada combinacion va contener r elementos, entonces,
-	// N-1 es la cantidad de combinaciones que pueden existir.
-	// stride: cual es el valor que tengo que agregar a la combinacion actual, comenzando desde
-	// el primer elemento de la lista.
-	if (r == 0)
-	{
-		// temp log
-		cout << "#" << counter << " " << c[0] << ", " << c[1] << endl;
-		counter++;
-		// r=0: no hay mas elementos pendientes por entrar a la combinacion, comparar valores
-		// si la suma de 'ambos' elementos en la combinacion es igual al TARGET, return true
-		if (testSum(c))
-		{
-			result = true;
-		}
-		return;
-	}
-	for (int i = stride; i <= list.size() - 1; ++i)
-	{
-		c.push_back(list[i]);
-		comparesum(list, i + 1, r - 1);
-		c.pop_back();
-	}
-}
-void exec_comparesum()
-{
-	vector<int> c;
-	vector<int> list = {1, 2, 3, 9};
-	comparesum(list, 0, 2);
-	if (result)
-	{
-		cout << "TRUE" << endl;
-	}
-	else
-	{
-		cout << "FALSE" << endl;
-	}
-	counter = 0;
-	list.clear();
-	c.clear();
-	list = {1, 2, 4, 4};
-	comparesum(list, 0, 2);
-	if (result)
-	{
-		cout << "TRUE" << endl;
-	}
-	else
-	{
-		cout << "FALSE" << endl;
-	}
+	return ((i != j) && (a[i] >= 0 && a[j] >= 0) && (a[i] == a[j]));
 }
 
-bool comparetwonumbers_itr(vector<int> &list, int target, int results[2])
+int PairNumbersItr(vector<int> &a)
 {
-	for (int i = 0; i < list.size(); ++i)
+	if (a.size() <= 1)
 	{
-		for (int j = i + 1; j < list.size(); ++j)
+		return 0;
+	}
+	int pares = 0;
+	for (int i = 0; i < a.size(); ++i)
+	{
+		for (int j = 1; j < a.size(); ++j)
 		{
-			const int a = list[i];
-			const int b = list[j];
-			const int sum = a + b;
-			if (sum == target)
+			//cout << i << " " << a[i] << " " << j << " " << a[j] << endl;
+			if (IsValidPair(a, i, j))
 			{
-				results[0] = a;
-				results[1] = b;
-				return true;
+				pares++;
+				a[i] = -1;
+				a[j] = -1;
+				continue;
 			}
 		}
 	}
-	return false;
-}
-void exec_comparetwonumbers()
-{
-	vector<int> list = {1, 2, 3, 9};
-	int results[] = {0, 0};
-	bool founded = comparetwonumbers_itr(list, 8, results);
-	if (founded)
-	{
-		cout << "founded in " << results[0] << ", " << results[1] << endl;
-	}
-	else
-	{
-		cout << "not founded!" << endl;
-	}
+	return pares;
 }
 
-void exec_printfrequencies()
+int PairNumbersCounter(vector<int> &a)
 {
-	/**
-	 * given a string of words, find frequencies of individual words.
-	 * */
-	const string str = "repeat after repeat word and another word";
-	stringstream ss(str);
-	unordered_map<string, int> mapWords;
-	string aWord;
-	while (ss >> aWord)
+	if (a.size() <= 1)
 	{
-		mapWords[aWord]++;
+		return 0;
 	}
-	for (unordered_map<string, int>::iterator wordItr = mapWords.begin(); wordItr != mapWords.end(); ++wordItr)
+	int pares = 0;
+	const int MAX_VALUE = 100;
+	int counter[MAX_VALUE] = {0};
+	for (int k = 0; k < a.size(); ++k)
 	{
-		cout << wordItr->first << " " << wordItr->second << endl;
+		counter[a[k]]++;
 	}
-}
-void unorderedMapExamples()
-{
-	/**
-	 * declaring map of type <string, int>
-	 * */
-	unordered_map<string, int> str2IntMap;
-	// insert values
-	str2IntMap["valueoften"] = 10;
-	str2IntMap["valueoftwenty"] = 20;
-	str2IntMap["valueofthirteen"] = 30;
-	// traversing the map
-	for (auto x : str2IntMap)
+	for (int i = 0; i <= MAX_VALUE; ++i)
 	{
-		cout << x.first << " " << x.second << endl;
+		if (counter[i] >= 2)
+		{
+			//cout << "Num. " << i << " count: " << counter[i] << endl;
+			if ((counter[i] % 2) == 0)
+			{
+				pares += counter[i] / 2;
+			}
+			else
+			{
+				pares += (counter[i] - 1) / 2;
+			}
+		}
 	}
-	/**
-	 * declaring map of type <string, float>
-	 * */
-	unordered_map<string, float> str2FloatMap;
-	// insert values
-	str2FloatMap["PI"] = 3.1416f;
-	str2FloatMap["root2"] = 1.414f;
-	str2FloatMap["root3"] = 1.732f;
-	str2FloatMap["log10"] = 2.302f;
-	str2FloatMap["logE"] = 1.0f;
-	//other insert method
-	str2FloatMap.insert(make_pair("e", 2.718f));
-	// test strings
-	string keyPI = "PI";
-	string keyLambda = "lambda";
-	// if key not found in map iterator to end is return
-	if (str2FloatMap.find(keyPI) == str2FloatMap.end())
-	{
-		cout << "\nNot founded: " << keyPI << endl;
-	}
-	else
-	{
-		cout << "\nFounded: " << keyPI << endl;
-	}
-	if (str2FloatMap.find(keyLambda) == str2FloatMap.end())
-	{
-		cout << "\nNot founded: " << keyLambda << endl;
-	}
-	else
-	{
-		cout << "\nFounded: " << keyLambda << endl;
-	}
-	// iterate over all map
-	cout << "All Elements:\n";
-	for (unordered_map<string, float>::iterator itrMap = str2FloatMap.begin(); itrMap != str2FloatMap.end(); ++itrMap)
-	{
-		cout << itrMap->first << " " << itrMap->second << endl;
-	}
+	return pares;
 }
 
-bool exec_comparetwonummap(vector<int> &list, const int target)
+int PairNumbersMap(vector<int> &a)
 {
-	unordered_map<int, int> mapList;
-	for (int i = 0; i < list.size(); ++i)
-	{
-		int valueKey = (list[i] - target) * -1;
-		if (mapList.find(valueKey) == mapList.end())
-		{
-			//mapList.insert(list[i],list[i]-target);
-			mapList[list[i]] = valueKey;
-		}
-		else
-		{
-			return true;
-		}
-	}
-	return false;
+	return 0;
 }
 
 int main()
 {
-	int target = 8;
-	vector<int> list = {1, 2, 4, 4};
-	const bool founded = exec_comparetwonummap(list, target);
+	//vector<int> a = {10, 20, 30, 20, 20, 50, 50, 10, 70};
+	vector<int> a = {10, 10, 10, 10, 10, 10, 10, 20, 20, 70, 70, 70, 70};
+	//int pares = PairNumbers(a);
+	int pares = PairNumbersCounter(a);
+	cout << "pares: " << pares << endl;
 	return EXIT_SUCCESS;
 }
