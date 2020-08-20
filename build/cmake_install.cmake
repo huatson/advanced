ed3009c31b9f5b326a99be0ba4827a97196a4fae
cesar Lopez <c.barreras@samsung.com>
2020-08-20 12:05:35 -0700

    updated changes

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/dev/advanced
+# Install script for directory: C:/dev/advanced/advanced
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/dev/advanced/install")
+  set(CMAKE_INSTALL_PREFIX "C:/dev/advanced/advanced/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,27 +34,24 @@ endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "D:/dev/advanced/build/Debug/advanced.exe")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/dev/advanced/advanced/build/Debug/advanced.exe")
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "D:/dev/advanced/build/Release/advanced.exe")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/dev/advanced/advanced/build/Release/advanced.exe")
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "D:/dev/advanced/build/MinSizeRel/advanced.exe")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/dev/advanced/advanced/build/MinSizeRel/advanced.exe")
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "D:/dev/advanced/build/RelWithDebInfo/advanced.exe")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/dev/advanced/advanced/build/RelWithDebInfo/advanced.exe")
   endif()
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES "D:/dev/advanced/build/advancedConfig.h")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES "C:/dev/advanced/advanced/build/advancedConfig.h")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE PROGRAM FILES
-    "C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Redist/MSVC/14.16.27012/x64/Microsoft.VC141.CRT/msvcp140.dll"
-    "C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Redist/MSVC/14.16.27012/x64/Microsoft.VC141.CRT/msvcp140_1.dll"
-    "C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Redist/MSVC/14.16.27012/x64/Microsoft.VC141.CRT/msvcp140_2.dll"
-    "C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Redist/MSVC/14.16.27012/x64/Microsoft.VC141.CRT/vcruntime140.dll"
-    "C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/VC/Redist/MSVC/14.16.27012/x64/Microsoft.VC141.CRT/concrt140.dll"
+    "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/redist/x64/Microsoft.VC120.CRT/msvcp120.dll"
+    "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/redist/x64/Microsoft.VC120.CRT/msvcr120.dll"
     )
 endif()
 
@@ -64,8 +61,8 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/dev/advanced/build/Libraries/CustomDFS/cmake_install.cmake")
-  include("D:/dev/advanced/build/Libraries/Combinatory/cmake_install.cmake")
+  include("C:/dev/advanced/advanced/build/Libraries/CustomDFS/cmake_install.cmake")
+  include("C:/dev/advanced/advanced/build/Libraries/Combinatory/cmake_install.cmake")
 
 endif()
 
@@ -77,5 +74,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/dev/advanced/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/dev/advanced/advanced/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/advanced.cpp
+++ src/advanced.cpp
@@ -13,117 +13,26 @@
 
 #include <unordered_map>
 
+bool findWords(char[][] wordMap, const char* word)
+{
+	return false;
+}
+
 /**
- *	a=[10,20,30,20,20,50,50,10,70]
- *	output=3
- *	vacio[]
- *	numeros enteros
- *	0<=a[i]<=100
- * 
- *	iterativo:
- *	a[i], a[j] >= 0 && i != j
- *	10 == 20 --> false
- *	10 == 30 --> false
- *		...
- *	10 == 10 --> true
- *	a=[-1,20,30,20,20,50,50,-1,70]
- *	20 == 30 --> false
- *	20 == 20 --> true
- *	a=[-1,-1,30,-1,20,50,50,-1,70]
- *	30 == 20 --> false
- *	30 == 50 --> false
- *	30 == 50 --> false
- *	30 == 70 --> false
- *	20 == 30 --> false
- *		...
- *	50 == 30 --> false
- *	50 == 20 --> false
- *	50 == 50 --> true
- *	a=[-1,-1,30,-1,20,-1,-1,-1,70]
- *	70 == 30 --> false
- *	70 == 20 --> false
- *
- * contador: 
- * crear contador c de a
- * (c >= 2)
- * 0 >= 2 --> false: NEXT
- * 2 >= 2 --> true: 2/2 = 1
- * 3 >= 2 --> true: (3-1)/2 = 1
- * 4 >= 2 --> true: 4/2= 2 
- * ..
- * 
- **/
-bool IsValidPair(vector<int> &a, const int i, const int j)
-{
-	return ((i != j) && (a[i] >= 0 && a[j] >= 0) && (a[i] == a[j]));
-}
-
-int PairNumbersItr(vector<int> &a)
-{
-	if (a.size() <= 1)
-	{
-		return 0;
-	}
-	int pares = 0;
-	for (int i = 0; i < a.size(); ++i)
-	{
-		for (int j = 1; j < a.size(); ++j)
-		{
-			//cout << i << " " << a[i] << " " << j << " " << a[j] << endl;
-			if (IsValidPair(a, i, j))
-			{
-				pares++;
-				a[i] = -1;
-				a[j] = -1;
-				continue;
-			}
-		}
-	}
-	return pares;
-}
-
-int PairNumbersCounter(vector<int> &a)
-{
-	if (a.size() <= 1)
-	{
-		return 0;
-	}
-	int pares = 0;
-	const int MAX_VALUE = 100;
-	int counter[MAX_VALUE] = {0};
-	for (int k = 0; k < a.size(); ++k)
-	{
-		counter[a[k]]++;
-	}
-	for (int i = 0; i <= MAX_VALUE; ++i)
-	{
-		if (counter[i] >= 2)
-		{
-			//cout << "Num. " << i << " count: " << counter[i] << endl;
-			if ((counter[i] % 2) == 0)
-			{
-				pares += counter[i] / 2;
-			}
-			else
-			{
-				pares += (counter[i] - 1) / 2;
-			}
-		}
-	}
-	return pares;
-}
-
-int PairNumbersMap(vector<int> &a)
-{
-	return 0;
-}
+ * matrix N  X N
+ * N = 5
+ * a b i j n
+ * e n z h n 
+ * i m p o q
+ * h w a n mo n b i h
+ * buscando 
+ * izpmi -> true
+ * enmwo -> false
+ * hibno -> true
+ * */
 
 int main()
 {
-	//vector<int> a = {10, 20, 30, 20, 20, 50, 50, 10, 70};
-	vector<int> a = {10, 10, 10, 10, 10, 10, 10, 20, 20, 70, 70, 70, 70};
-	//int pares = PairNumbers(a);
-	int pares = PairNumbersCounter(a);
-	cout << "pares: " << pares << endl;
+
 	return EXIT_SUCCESS;
 }
