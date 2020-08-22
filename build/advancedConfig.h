dee26d64f9ad43e86c7c553e49c1252cc8cda6ea
huatson <huatson@live.com>
2020-08-21 21:04:50 -0700

    updated change

--- build/advancedConfig.h
+++ build/advancedConfig.h
@@ -1,6 +1,10 @@
 #ifndef __ADVANCED_CONFIG_H__
 #define __ADVANCED_CONFIG_H__
 
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #define advanced_VERSION_MAJOR  1
 #define advanced_VERSION_MINOR  0
 
--- src/advanced.cpp
+++ src/advanced.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include "advancedConfig.h"
 
-#include <vector>
+#include "advancedConfig.h"
 
 #ifdef USE_DFS
 #include "CustomDFS.h"
@@ -11,12 +9,10 @@
 #include "Combinatory.h"
 #endif
 
+#include <iostream>
+#include <vector>
 #include <unordered_map>
-
-bool findWords(char[][] wordMap, const char* word)
-{
-	return false;
-}
+#include <unordered_set>
 
 /**
  * matrix N  X N
@@ -24,15 +20,70 @@ bool findWords(char[][] wordMap, const char* word)
  * a b i j n
  * e n z h n 
  * i m p o q
- * h w a n mo n b i h
+ * h w a n m
+ * o n b i h
  * buscando 
  * izpmi -> true
  * enmwo -> false
  * hibno -> true
+ * 
+ * iterative
+ * izpmi = 5 
+ * i == a --> false
+ * i == b --> false
+ * i == i --> true
  * */
+static const int N = 5;
+static char EMPTY = '-';
+bool findWords(char wordMap[N][N], const char *word)
+{
+	if (strlen(word) > (N * N))
+	{
+		return false;
+	}
+	int idx = 0;
+	bool letterFounded = false;
+	int counterFounded = 0;
+	while (idx < strlen(word))
+	{
+		letterFounded = false;
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			{
+				cout << word[idx] << " " << wordMap[i][j] << endl;
+				if ((wordMap[i][j] != EMPTY) && (wordMap[i][j] == word[idx]))
+				{
+					counterFounded++;
+					wordMap[i][j] = EMPTY;
+					letterFounded = true;
+					break;
+				}
+			}
+			if (letterFounded)
+			{
+				break;
+			}
+		}
+		idx++;
+	}
+	cout << "founded: " << counterFounded << " of " << strlen(word) << endl;
+	if(counterFounded == strlen(word))
+	{
+		return true;
+	}
+	return false;
+}
 
 int main()
 {
-
+	FILE *f = freopen("src/input.txt", "r", stdin);
+	if (!f)
+		return EXIT_FAILURE;
+	char data[N][N] = {'a'};
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
+			cin >> data[i][j];
+	bool founded = findWords(data, "izcxi");
 	return EXIT_SUCCESS;
 }
